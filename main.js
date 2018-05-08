d33d750fe81330d0283bdc4a36c7474b5385deb3
Michelle S Lee <michlee1337@gmail.com>
2018-05-08 15:43:44 +0800

    basic frontend of app

---
+++ main.js
@@ -0,0 +1,14 @@
+// Popover
+
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();
+});
+
+/*import fake.js;
+
+int count = tdata.length;
+
+if count <15 {
+  $(#progressTable).class= "col-xs-3";
+}
+*/
