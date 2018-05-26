8ea862cf3f347731b40c1581f6ccc73455ebca0d
Michelle S Lee <michlee1337@gmail.com>
2018-05-26 21:40:15 +0800

    changed js to get last git commit

--- main.js
+++ main.js
@@ -2,6 +2,14 @@
 
 $(document).ready(function(){
     $('[data-toggle="popover"]').popover();
+
+    // get last commit
+    var git = require('git-last-commit');
+
+    git.getLastCommit(function(err, commit) {
+      // read commit object properties
+      console.log(commit);
+    });
 });
 
 /*import fake.js;
